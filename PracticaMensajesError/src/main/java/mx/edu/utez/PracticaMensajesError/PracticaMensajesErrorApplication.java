b6d0b0e3a685cacf73b95a6bc4092d71e4425df8
LeoDoCa <20223tn049@utez.edu.mx>
2025-02-12 09:25:26 -0600

    Agregue mi trabajo al repositorio

---
+++ PracticaMensajesError/src/main/java/mx/edu/utez/PracticaMensajesError/PracticaMensajesErrorApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.PracticaMensajesError;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaMensajesErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaMensajesErrorApplication.class, args);
+	}
+
+}
---
+++ PracticaMensajesError/src/main/java/mx/edu/utez/PracticaMensajesError/config/InitialConfig.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.PracticaMensajesError.config;
+
+public class InitialConfig {
+}
