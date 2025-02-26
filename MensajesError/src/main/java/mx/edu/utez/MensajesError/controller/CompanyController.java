0e93c5bb42e3b9fb5721bccf33a2c6f2c86f35c0
LeoDoCa <20223tn049@utez.edu.mx>
2025-02-26 12:11:51 -0600

    Agregue lo de la tarea para el 26 de febrero

--- MensajesError/src/main/java/mx/edu/utez/MensajesError/controller/CompanyController.java
+++ MensajesError/src/main/java/mx/edu/utez/MensajesError/controller/CompanyController.java
@@ -6,7 +6,8 @@ import mx.edu.utez.MensajesError.controller.dto.CompanyDto;
 import mx.edu.utez.MensajesError.service.CompanyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.UUID;
 
 
@@ -14,32 +15,38 @@ import java.util.UUID;
 @RequestMapping("/api/company")
 public class CompanyController {
     private final CompanyService service;
-
+    private static final Logger logger = LogManager.getLogger(CompanyController.class);
     public CompanyController(CompanyService service) {
         this.service = service;
     }
 
     @GetMapping("/")
     public ResponseEntity<ApiResponse> getAll() {
+        logger.info("Solicitud para obtener todas las compañías");
         return service.findAll();
     }
 
     @PostMapping("/")
     public ResponseEntity<ApiResponse> register(@Valid @RequestBody CompanyDto dto) {
-        System.out.println(dto);
+        logger.info("Solicitud para registrar una nueva compañía: {}", dto);
         return service.save(dto.toEntity());
     }
 
     @PutMapping("/")
     public ResponseEntity<ApiResponse> update(@RequestBody CompanyDto dto) {
+        logger.info("Solicitud para actualizar compañía: {}", dto);
         return service.update(dto.toEntity());
     }
 
     @GetMapping("/{uuid}")
-    public ResponseEntity<ApiResponse> getByUuid(@PathVariable UUID uuid) { return service.findByUuid(uuid);}
+    public ResponseEntity<ApiResponse> getByUuid(@PathVariable UUID uuid) {
+        logger.info("Solicitud para obtener compañía con UUID: {}", uuid);
+        return service.findByUuid(uuid);
+    }
 
     @DeleteMapping("/{uuid}")
     public ResponseEntity<ApiResponse> delete(@PathVariable UUID uuid){
+        logger.warn("Solicitud para eliminar compañía con UUID: {}", uuid);
         return service.delete(uuid);
     }
 }
